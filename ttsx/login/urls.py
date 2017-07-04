fc51f312217dfb1d8d0eaaa48db5fe49873b24c3
flymrc <flymrc@qq.com>
2017-07-04 23:28:04 +0800

    shell auto commit

--- ttsx/login/urls.py
+++ ttsx/login/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from login import views
 urlpatterns = [
     url(r'^login/$', views.login),
+    url(r'^login_check/$', views.login_check),
     url(r'^index/$', views.index),
     url(r'^register/$', views.register),
 ]
--- ttsx/login/views.py
+++ ttsx/login/views.py
@@ -1,9 +1,25 @@
+#coding=utf-8
+
 from django.shortcuts import render
-from
+from django.http import HttpResponse
+from models import UserInfo
 # Create your views here.
 
 def login(request):
     return render(request, 'login.html')
+
+def login_check(request):
+    uname = request.POST.get('username')
+    passwd = request.POST.get('pwd')
+    urow = UserInfo.objects.get(uname)
+    if urow:
+        if urow.password == passwd:
+            return HttpResponse('ok')
+        else:
+            return HttpResponse('password error')
+    else:
+        return HttpResponse('请注册')
+
 
 def index(request):
     return render(request, 'index.html')
