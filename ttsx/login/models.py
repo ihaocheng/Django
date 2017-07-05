c419a7470820bc48c94cfe326c8cdb3e5fac2662
flymrc <flymrc@qq.com>
2017-07-05 09:18:57 +0800

    shell auto commit

--- ttsx/login/models.py
+++ ttsx/login/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
-    password = models.CharField(max_length=26)
+    password = models.CharField(max_length=40)#sha1
     mail = models.CharField(max_length=50, null=True, blank=True)
     tel = models.CharField(max_length=20, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
