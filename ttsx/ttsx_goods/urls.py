d0d13338e31bfe8f2d433bb0ec71c4c66328a154
flymrc <flymrc@qq.com>
2017-07-09 03:11:25 +0800

    列表页差页码

--- ttsx/ttsx_goods/urls.py
+++ ttsx/ttsx_goods/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include, patterns
 from ttsx_goods import views
 
 
 urlpatterns = [
     url(r'^$',views.index),
     url(r'^index/$',views.index),
-    url(r'^list(\d*)/$',views.goods_list),
+    url(r'^list(\d{0,1})(\d*)',views.goods_list),
+    url(r'^tag/$',views.tag),
 
 ]
--- ttsx/ttsx_goods/views.py
+++ ttsx/ttsx_goods/views.py
@@ -18,21 +18,45 @@ def index(request):
     context = {'uname':uname, 'top':'1', 'title':'首页', 'alist':alist}
     return render(request, 'goods/index.html', context)
 
-def goods_list(request, tid, pid):
-    uname = request.session.get('uname')
-    context = {'uname':uname, 'top':'1'}
+def goods_list(request, tid, pindex):
+    if not pindex:  # 分页索引值，默认为1
+        pindex = 1
+    url = request.path # 取得不带参数的路径
+    uname = request.session.get('uname') # js判断登陆状态
+    context = {'uname':uname, 'top':'1', 'url':url}
+
+    tag = request.session.get('tag')
+
+    print ("list: %s" % tag)
+    if tag == 'price':
+        ord_str = 'gprice'
+    elif tag == 'click':
+        ord_str = '-gclick'
+    else:
+        ord_str = '-id'
+
+    # 无tid为全部分类
     if not tid:
-        goods_list = GoodsInfo.objects.all()
+        goods_list = GoodsInfo.objects.all().order_by(ord_str)
+        goods_list2 = GoodsInfo.objects.all().order_by('-id')
     else:
         t = TypeInfo.objects.get(pk=int(tid))
-        goods_list = t.goodsinfo_set.order_by('-id')
+        goods_list = t.goodsinfo_set.order_by(ord_str)
+        goods_list2 = t.goodsinfo_set.order_by('-id')
         context['t'] = t
 
-    new_goods = goods_list[:2]
+    new_goods = goods_list2[:2]
     context['new_goods'] = new_goods
 
-    paginator = Paginator(goods_list, 14)
-    goods_page = paginator.page(1)
+    paginator = Paginator(goods_list, 15)
+    goods_page = paginator.page(int(pindex))
     context['goods'] = goods_page
-
     return render(request, 'goods/list.html', context)
+
+def tag(request):
+    tag = request.GET.get('tag')
+    print("tag: " % tag)
+
+    request.session['tag'] = tag
+
+
