419e7360f4c23dbedc2e3fbc7f77e441cf439a6e
flymrc <flymrc@qq.com>
2017-07-11 22:54:49 +0800

    创建cart models

--- ttsx/ttsx_cart/models.py
+++ ttsx/ttsx_cart/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
+class CartInfo(models.Model):
+    goods = models.ForeignKey(to='ttsx_goods.GoodsInfo')
+    user = models.ForeignKey(to='ttsx_user.UserInfo')
+    count = models.IntegerField()
