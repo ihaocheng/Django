222df0bb32126464e916eaf0a971d7e23bf65057
flymrc <flymrc@qq.com>
2017-07-12 16:07:15 +0800

    detail 添加购物车动画

--- ttsx/ttsx_cart/urls.py
+++ ttsx/ttsx_cart/urls.py
@@ -3,5 +3,6 @@ import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^add/$', views.add),
 
 ]
--- ttsx/ttsx_cart/views.py
+++ ttsx/ttsx_cart/views.py
@@ -1,4 +1,8 @@
+#coding=utf-8
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from models import CartInfo
 
 # Create your views here.
 
@@ -7,3 +11,12 @@ def index(request):
     context = {'uname':uname, }
     return render(request, 'cart/cart.html', context)
 
+def add(request):
+    try:
+        count = request.GET.get('count')
+        gid = request.GET.get('gid')
+
+
+        return JsonResponse({'response':'1'})
+    except:
+        return JsonResponse({'response':'0'})
